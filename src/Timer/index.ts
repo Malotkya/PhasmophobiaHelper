0ce9c70599b4e4c16aa70fdf441262c558690cfa
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-09-18 16:49:31 -0500

    Display Timers on page.

--- src/Timer/index.ts
+++ src/Timer/index.ts
@@ -1,10 +1,19 @@
-import { getClock } from "./Clock";
+import getClock from "./Clock";
+import Timer, {createAllTimers} from "./Timer";
+import StopWatch from "./StopWatch";
+import HuntTimer from "./HuntTimer";
 
 export default async function createTimer(): Promise<HTMLElement>{
     const element = document.createElement("div");
     element.id = "timer-main";
 
-    element.appendChild(getClock());
+    const table = document.createElement("table");
+    createAllTimers(table);
+    new HuntTimer(table);
+    new StopWatch(table);
 
+    element.appendChild(getClock());
+    element.appendChild(table);
+    
     return element;
 }
