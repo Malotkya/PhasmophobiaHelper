52819ef4ea3bc9c87ae5660d49d0271ed0831b55
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-09-18 16:48:32 -0500

    Update Clock for timers.

--- src/Timer/Clock.ts
+++ src/Timer/Clock.ts
@@ -1,29 +1,90 @@
+/** Clock.ts
+ * 
+ * @author Alex Malotky
+ */
+
+//Clock Elements viewd on page.
 const clock = document.createElement("span");
+    clock.id = "clock";
+    clock.style.fontFamily = "monospace";
 
+/** Task Interface
+ * 
+ */
 export interface Task{
-    index: number,
-    type: string,
-    description: string,
-    endTime: number,
-    delete: number
+    id: string,
+    update:(time:number)=>boolean
 }
-let queue: Array<Task> = [];
 
-const refreshRate = 250;
+//Queue of Tasks
+const queue: Set<Task> = new Set();
+
+// Clock thread that updates clock and queue.
+const REFRESH_RATE = 100;
 function clockThread(): void{
-    let now = new Date(Date.now());
-    let h:string = `0${now.getHours() % 12}`;
-    let m:string = `0${now.getMinutes()}`;
-    let s:string = `0${now.getSeconds()}`;
+    let now:number = Date.now();
+    clock.textContent = formatTime(now);
 
-    clock.textContent = `${h.slice(-2)}:${m.slice(-2)}:${s.slice(-2)}`;
+    //Launch any schuduled tasks
+    let index = 0;
+    queue.forEach((task:Task)=>{
+        if(task.update(now))
+            queue.delete(task);
+    });
 
-    //Delete any tasks
-    queue = queue.filter((task:Task)=>task.delete < now.valueOf());
+    window.setTimeout(clockThread, REFRESH_RATE);
+} clockThread();
 
-    window.setTimeout(clockThread, refreshRate);
-}clockThread();
+/** Add Task to Queue
+ * 
+ * @param {Task} t 
+ */
+export function addTask(t:Task){
+    queue.add(t);
+}
 
-export function getClock(){
+/** Format Time
+ * 
+ * Returns "00:00:00" if nothing entered.
+ * 
+ * @param {number|Date} t 
+ * @returns {string}
+ */
+export function formatTime(t?: number|Date):string{
+    
+    if(typeof t === "undefined"){
+        return "00:00:00";
+    } else if(typeof t === "number"){
+        t = new Date(t);
+    }
+
+    let h:string = `0${t.getHours() % 12 || 12}`;
+    let m:string = `0${t.getMinutes()}`;
+    let s:string = `0${t.getSeconds()}`;
+
+    return `${h.slice(-2)}:${m.slice(-2)}:${s.slice(-2)}`;
+}
+
+/** Format Seconds
+ * 
+ * @param {number} value 
+ * @returns {string}
+ */
+export function formatSeconds(value: number): string{
+    const m:number = Math.floor(value / 60);
+    const s:number = Math.round(value % 60);
+
+    if(m === 0){
+        return value.toFixed(1);
+    }
+    
+    return `${m}:${s}`;
+}
+
+/** Get Clock Html Element
+ * 
+ * @returns {HTMLElement}
+ */
+export default function getClock(): HTMLElement{
     return clock;
 }
