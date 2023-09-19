e65d6e097e2dd2aa90a461f571eadf4d2f3508d1
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-09-19 10:34:53 -0500

    Switched to non async/ static method.

--- src/CheckList/index.ts
+++ src/CheckList/index.ts
@@ -8,64 +8,68 @@ import Ghost, {createAllGhosts} from "./Ghost";
 import Phasmophobia from "./Phasmophobia";
 import { generateSound } from "../Util/Sound";
 
-export default async function createCheckList(): Promise<HTMLElement>{
-    const element = document.createElement("div");
-    element.id = "checklist-main";
+//Main Check List Element
+const checkList = document.createElement("div");
+    checkList.id = "checklist-main";
 
-    const [evidenceSectionElement, evidenceListElement] = createEvidenceListElement();
-    const [ghostSectionElement, ghostListElement] = createGhostListElement();
-    const [displaySectionElement, displayTargetElement] = createDisplayTargetElement();
+//Other Check List Elements
+const [evidenceSectionElement, evidenceListElement] = createEvidenceListElement();
+const [ghostSectionElement, ghostListElement] = createGhostListElement();
+const [displaySectionElement, displayTargetElement] = createDisplayTargetElement();
 
-    const ghostList = await createAllGhosts(ghostListElement, displayTargetElement);
-
+createAllGhosts(ghostListElement, displayTargetElement).then((ghostList:Array<Ghost>)=>{
     //Display First Ghost
     ghostList[0].display();
 
-    const evidenceList = await createAllEvidence(evidenceListElement);
-        
-    //Display Everything
-    element.appendChild(evidenceSectionElement);
-    element.appendChild(ghostSectionElement);
-    element.appendChild(displaySectionElement);
+    createAllEvidence(evidenceListElement).then((evidenceList:Array<Evidence>)=>{
+        //Display Everything
+        checkList.appendChild(evidenceSectionElement);
+        checkList.appendChild(ghostSectionElement);
+        checkList.appendChild(displaySectionElement);
 
-    //Create Inputs
-    const [numEvidence, btnReset] = createInputElements(evidenceSectionElement);
-    const alternativeList = addAlternativeElements(evidenceSectionElement);
+        //Create Inputs
+        const [numEvidence, btnReset] = createInputElements(evidenceSectionElement);
+        const alternativeList = addAlternativeElements(evidenceSectionElement);
 
-    //Create Game
-    const game = new Phasmophobia(evidenceList, ghostList, alternativeList, ghostListElement);
-    
-    //Evidence Number Change Event
-    numEvidence.addEventListener("change", event=>{
-        game.evidenceCount = Number(numEvidence.value);
-        game.update();
-    });
-    numEvidence.value = game.evidenceCount.toString();
-    
-    //Reset Event
-    btnReset.addEventListener("click", event=>{
-        event.stopPropagation();
-        game.reset();
-    });
-
-    //Update Event
-    evidenceSectionElement.addEventListener("click", event=>{
-        game.update();
-    });
-
-    //Sound Event
-    displayTargetElement.addEventListener("click", event=>{
-        const target:Element = (<Element>event.target);
-        if(target.className === "speed"){
+        //Create Game
+        const game = new Phasmophobia(evidenceList, ghostList, alternativeList, ghostListElement);
+            
+        //Evidence Number Change Event
+        numEvidence.addEventListener("change", event=>{
+            game.evidenceCount = Number(numEvidence.value);
+            game.update();
+        });
+        numEvidence.value = game.evidenceCount.toString();
+            
+        //Reset Event
+        btnReset.addEventListener("click", event=>{
             event.stopPropagation();
-            const speed: number = Number(target.getAttribute("value"));
-            if(isNaN(speed)){
-                console.warn(target.getAttribute("value") + "is not a number!");
-            } else {
-                generateSound(speed);
-            }
-        }
-    })
+            game.reset();
+        });
 
-    return element;
+        //Update Event
+        evidenceSectionElement.addEventListener("click", event=>{
+            game.update();
+        });
+
+        //Sound Event
+        displayTargetElement.addEventListener("click", event=>{
+            const target:Element = (<Element>event.target);
+            if(target.className === "speed"){
+                event.stopPropagation();
+                const speed: number = Number(target.getAttribute("value"));
+                if(isNaN(speed)){
+                    console.warn(target.getAttribute("value") + "is not a number!");
+                } else {
+                    generateSound(speed);
+                }
+            }
+        });
+
+    }); //End Create Evidence List
+
+}); //End Create Ghost List
+
+export default function CheckList(): HTMLElement{
+    return checkList;
 }
