8b6e087d38b8d2499b915c4f1d22af193aeb929e
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-09-19 10:35:49 -0500

    Changes to incorporate new elements.

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,17 @@
-import createCheckList from "./CheckList";
-import createTimer from "./Timer";
+import CheckList from "./CheckList";
+import Timer from "./Timer";
 import makeInterface from "./Util/Sound";
+import getClock from "./Timer/Clock";
+import App from "./Pages";
 
-window.onload = () => {
+const app = new App();
 
-    //Find Main Element
-    const main: HTMLElement = document.querySelector("main");
+app.add("", CheckList());
+app.add("Check List", CheckList());
+app.add("Timer", Timer());
+
+app.onReady(()=>{
     const header: HTMLElement = document.querySelector("header");
-    main.innerHTML = "";
-
-    createCheckList().then(checkList=>{
-
-        createTimer().then(timer=>{
-
-            main.appendChild(checkList);
-            main.appendChild(timer);
-            
-            header.appendChild(makeInterface());
-        });
-    });
-}
+    header.appendChild(makeInterface());
+    header.appendChild(getClock());
+});
