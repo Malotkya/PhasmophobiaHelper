06daf2e926c841f7f739625d8a1715015b6ce0fe
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-09-19 10:35:13 -0500

    Switched to non async/static method

--- src/Timer/index.ts
+++ src/Timer/index.ts
@@ -1,19 +1,17 @@
-import getClock from "./Clock";
-import Timer, {createAllTimers} from "./Timer";
+import {createAllTimers} from "./Timer";
 import StopWatch from "./StopWatch";
 import HuntTimer from "./HuntTimer";
 
-export default async function createTimer(): Promise<HTMLElement>{
-    const element = document.createElement("div");
-    element.id = "timer-main";
+const timer = document.createElement("div");
+    timer.id = "timer-main";
 
-    const table = document.createElement("table");
-    createAllTimers(table);
-    new HuntTimer(table);
-    new StopWatch(table);
+const list = document.createElement("ul");
+createAllTimers(list);
+new HuntTimer(list);
+new StopWatch(list);
 
-    element.appendChild(getClock());
-    element.appendChild(table);
-    
-    return element;
+timer.appendChild(list);
+
+export default function Timer(): HTMLElement{
+    return timer;
 }
