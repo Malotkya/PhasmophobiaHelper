9247b640293c293df6d57320b33aeb5a3d9c6ef1
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-09-25 16:04:11 -0500

    Fixed styling issues

--- src/Util/HTMLToggleInput.ts
+++ src/Util/HTMLToggleInput.ts
@@ -1,80 +1,40 @@
 export default class HTMLToggleInputElement extends HTMLElement {
-    private _switch: HTMLLabelElement;
     private _slider: HTMLSpanElement;
     private _input: HTMLInputElement;
-    private _style: HTMLStyleElement;
 
     private _options: Array<string>
-    private _value: string;
 
     constructor(option1?:string, option2?: string){
         super();
 
-        this._style = document.createElement("style");
-        this._style.innerHTML = `
-        .switch {
-            position: relative;
-            display: inline-block;
-            width: 60px;
-            height: 34px;
-          }
-          
-          .switch input {
-            opacity: 0;
-            width: 0;
-            height: 0;
-          }
-          
-          .slider {
-            position: absolute;
-            cursor: pointer;
-            top: 0;
-            left: 0;
-            right: 0;
-            bottom: 0;
-            background-color: #ccc;
-            border-radius: 34px;
-            -webkit-transition: .4s;
-            transition: .4s;
-          }
-          
-          .slider:before {
-            position: absolute;
-            content: "";
-            height: 26px;
-            width: 26px;
-            left: 4px;
-            bottom: 4px;
-            background-color: white;
-            -webkit-transition: .4s;
-            transition: .4s;
-          }
-          
-          input:checked + .slider {
-            background-color: #2196F3;
-          }
-          
-          input:focus + .slider {
-            box-shadow: 0 0 1px #2196F3;
-          }
-          
-          input:checked + .slider:before {
-            -webkit-transform: translateX(26px);
-            -ms-transform: translateX(26px);
-            transform: translateX(26px);
-          }
-        `;
-        this._switch = document.createElement("label");
-        this._switch.className = "switch";
-        this._input = document.createElement("input");
-        this._input.type = "check";
-        this._slider = document.createElement("span");
-        this._slider.className = "slider";
+        this.style.position = "relative";
+        this.style.display = "inline-block";
+        this.style.width = "60px";
+        this.style.height = "34px";
+        this.style.borderRadius = "34px";
+        this.style.backgroundColor = "white";
+        this.style.cursor = "pointer";
 
-        this.appendChild(this._style);
-        this.appendChild(this._switch);
-        this._switch.appendChild(this._input);
-        this._switch.appendChild(this._slider);
+        this._input = document.createElement("input");
+        this._input.style.opacity = "0";
+        this._input.style.width = "0";
+        this._input.style.height = "0";
+
+        this._slider = document.createElement("span");
+        this._slider.style.content = "";
+        this._slider.style.position = "absolute";
+        this._slider.style.top = "4px";
+        this._slider.style.height = "26px"
+        this._slider.style.height = "26px";
+        this._slider.style.width = "26px";
+        this._slider.style.left = "4px";
+        this._slider.style.bottom = "4px";
+        this._slider.style.backgroundColor = "#444";
+        this._slider.style.borderRadius = "34px";
+        this._slider.style.transition = "400ms";
+
+        this.appendChild(this._input);
+        this.appendChild(this._slider);
 
         this._options = [option1, option2];
 
@@ -83,6 +43,8 @@ export default class HTMLToggleInputElement extends HTMLElement {
                 event.stopPropagation();
             } else {
                 this._input.checked = !this._input.checked;
+                this._slider.style.left = this._input.checked? "26px" : "4px";
+                this.style.backgroundColor = this._input.checked? "cyan" : "white";
                 this.dispatchEvent(new Event("change", {composed: true}));
             }
         });
