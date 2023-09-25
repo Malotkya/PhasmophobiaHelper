f40aa7e98961e8241c63faf5e79c97d4fdeb8782
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-09-25 14:59:52 -0500

    Attempting to fix unable to change volume.

--- src/Util/Sound/Fallback.ts
+++ src/Util/Sound/Fallback.ts
@@ -1,39 +1,53 @@
-import { sound_interface } from ".";
+/** Fallback.ts
+ * 
+ * Fallback Audio using original bleep.
+ * 
+ * Help From:
+ * https://stackoverflow.com/questions/48938140/cross-browser-html5-javascript-generate-single-tick-sound
+ */
+import { audio_interface } from ".";
 
 const audioContext = new (window.AudioContext)();
 export const TIC_FREQUENCEY: number = 103;
 export const TIC_LENGTH: number = 0.03;
 
-export class Fallback implements sound_interface{
-    private _gain:any;
-    private static error: OnErrorEventHandler;
-    public currentTime: number;
+export class Fallback implements audio_interface{
+    private _masterGain: any;
+    private _nodeGain: any;
 
     constructor(volume?: string|number){
-        this._gain = audioContext.createGain();
-        this._gain.connect(audioContext.destination);
+        this._masterGain = audioContext.createGain();
+        this._masterGain.connect(audioContext.destination);
+
+        this._nodeGain = audioContext.createGain();
+        this._nodeGain.connect(this._masterGain);
 
         if(volume)
             this.volume = Number(volume);
     }
 
-    get volume(){
-        return this._gain.value;
+    get volume(): number{
+        return this._masterGain.value;
     }
 
     set volume(value: number){
-        this._gain.value = value;
+        this._masterGain.value = value;
+        this._nodeGain.value = value;
     }
 
     async play(){
         var oscillatorNode = new OscillatorNode(audioContext, {type: 'sawtooth'});
         oscillatorNode.frequency.value = TIC_FREQUENCEY;
-        oscillatorNode.connect( this._gain );
+        oscillatorNode.connect( this._nodeGain );
         oscillatorNode.start(audioContext.currentTime);
-        oscillatorNode.stop( audioContext.currentTime + TIC_LENGTH);
+        oscillatorNode.stop(audioContext.currentTime + TIC_LENGTH);
     }
 
-    get onerror(){
-        return Fallback.error;
+    get currentTime():number{
+        return audioContext.currentTime;
+    }
+
+    set currentTime(value:number){
+        //Do Nothing
     }
 }
