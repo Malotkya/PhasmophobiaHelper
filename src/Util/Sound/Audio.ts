a46f292dd8eb69dea753ca0412f46e19a7896224
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-09-26 09:55:29 -0500

    Moved audio interface to its own file.

---
+++ src/Util/Sound/Audio.ts
@@ -0,0 +1,37 @@
+/** Sound Interface
+ * 
+ * Contains elements of Audio that is required by this file.
+ */
+import { Fallback } from "./Fallback";
+
+export interface audio_interface {
+    play: ()=>Promise<void>;
+    volume: number,
+    onerror?: OnErrorEventHandlerNonNull,
+    cloneNode: ()=>any
+}
+
+//Audio Interface Object
+let audio:audio_interface = new Fallback();
+
+/** Set Audio File
+ * 
+ * If the file fails to load, will use fallback bleep.
+ * 
+ * @param {string} fileName 
+ */
+export function setAudioFile(fileName:string){
+    audio = new Audio(fileName);
+    audio.onerror = () => audio = new Fallback();
+}
+
+/** Play Audio File
+ * 
+ * 
+ * @param {number} volume 
+ */
+export function playAudio(volume: number){
+    const temp:audio_interface = audio.cloneNode();
+    temp.volume = volume;
+    temp.play();
+}
