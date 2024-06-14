2876c5c68ec1658a4c02b08f5d06e2e068715289
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-06-14 08:53:11 -0500

    moved final build to seperate file.

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,22 @@
 const path = require('path');
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 
 //test if in production environment
 const prod = process.argv.includes('prod');
 
+//Main Directories
+const build_directory  = path.join(__dirname, "build");
+const source_directory = path.join(__dirname, "src");
+const public_directory = path.join(__dirname, "public");
+
 module.exports = {
     mode: prod? "production": "development",
     entry: {
         index: [
-            path.join(__dirname, "src", "index.ts"),
-            path.join(__dirname, "src", "style.scss")
+            path.join(source_directory, "index.ts"),
+            path.join(source_directory, "style.scss")
         ]
     },
     devtool: prod?  undefined: 'source-map',
@@ -41,11 +47,19 @@ module.exports = {
     },
     output: { 
         filename: '[name].js',
-        path: path.join(__dirname, "public"),
+        path: build_directory,
         library: {
             type: 'module'
         }
     },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [{
+                to: build_directory,
+                from: public_directory
+            }]
+        })
+    ],
     optimization: {
         minimize: prod,
         minimizer:[
