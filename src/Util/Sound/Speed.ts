762ce533396e685b834f4c5b83cdb50c104c063b
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-12-16 14:31:58 -0600

    Added incerse speed equations.

--- src/Util/Sound/Speed.ts
+++ src/Util/Sound/Speed.ts
@@ -19,6 +19,18 @@ const KNOWN_SPEEDS = new Map<number, number>();
     KNOWN_SPEEDS.set(1.0 ,  64);
     KNOWN_SPEEDS.set(0.4 ,  10);
 
+/** Inverse Known Speeds Get
+ * 
+ * @param {number} value 
+ * @returns {number}
+ */
+function inverseKnownSpeedsGet(value:number):number|undefined {
+    for(const [mps, bpm] of KNOWN_SPEEDS.entries()) {
+        if(value >= bpm-2.5 && value <= bpm+2.5)
+            return mps;
+    }
+}
+
 /** In Beats per Minute Equation
  * 
  * Cubic Equation generated on Wolfram Alpha using points above.
@@ -27,6 +39,15 @@ const KNOWN_SPEEDS = new Map<number, number>();
  * @returns {number} in bpm
  */
 const bpm_equation = (speed: number):number => -29.5033 + (108.606 * speed) - (22.2689 * Math.pow(speed, 2)) + (4.93617 * Math.pow(speed, 3));
+
+/** In Meters per Second Equation
+ * 
+ * Cubic Equation generated on Wolfram Alpha using points above.
+ * 
+ * @param {number} speed in bpm
+ * @returns {number} in m/s
+ */
+const mps_equation = (speed: number):number => 0.283168 + (0.0104645 * speed) + (0.0000258484 * Math.pow(speed, 2)) - (0.0000000867289 * Math.pow(speed, 3));
 
 /** In Beats per Minute
  * 
@@ -37,9 +58,44 @@ const bpm_equation = (speed: number):number => -29.5033 + (108.606 * speed) - (2
  */
 export const bpm = (speed: number):number => KNOWN_SPEEDS.get(speed) || bpm_equation(speed);
 
+/** In Meeters per Second
+ * 
+ * Uses Reference Map or Equation.
+ * 
+ * @param {number} speed in bpm
+ * @returns {number} in m/s
+ */
+export const mps = (speed: number):number => inverseKnownSpeedsGet(speed) || mps_equation(speed);
+
 /** Convert to Microseconds
  * 
  * @param {number} speed in bpm
  * @returns {number} im bpMs
  */
 export const convert = (speed: number):number => (60 / speed) * 1000;
+
+/** Convert to Microseconds
+ * 
+ * @param {number} speed in bpMs
+ * @returns {number} im bpm
+ */
+export const inverseConvert = (speed: number):number => (speed / 1000) / 60;
+
+/** Get Classification
+ * 
+ * @param {number} speed in bpm
+ * @returns {string}
+ */
+export function getSpeedClass(value:number) {
+    const average = KNOWN_SPEEDS.get(1.7);
+
+    if(value === 0){
+        return "Unknown";
+    }else if(value < average - 8) {
+        return "Slow";
+    } else if(value < average + 8){
+        return "Average";
+    } else {
+        return "Fast";
+    }
+}
